200789288fe8cdf3aa47497bd0bbab7937865018
KM <keltoumimohamed268@gmail.com>
2026-04-02 01:16:31 +0200

    use env vars for supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://sbpilyghmvmoechmarrt.supabase.co'
-const supabaseKey = 'sb_publishable_YljFArsmAPvtcxri3XZ40w_e7MRUTYT'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
